ecd5d5a6a63718a1f2424f710848d3c9da9a1b40
Arwaz Jamal <arwaz.jamal@codinova.com>
2021-01-18 14:00:12 +0530

    add functionality for list update to add tab and rmv tab using settings

--- src/index.js
+++ src/index.js
@@ -75,6 +75,18 @@ class List {
           '<svg width="17" height="13" viewBox="0 0 17 13" xmlns="http://www.w3.org/2000/svg"><path d="M5.819 4.607h9.362a1.069 1.069 0 0 1 0 2.138H5.82a1.069 1.069 0 1 1 0-2.138zm0-4.607h9.362a1.069 1.069 0 0 1 0 2.138H5.82a1.069 1.069 0 1 1 0-2.138zm0 9.357h9.362a1.069 1.069 0 0 1 0 2.138H5.82a1.069 1.069 0 0 1 0-2.137zM1.468 4.155V1.33c-.554.404-.926.606-1.118.606a.338.338 0 0 1-.244-.104A.327.327 0 0 1 0 1.59c0-.107.035-.184.105-.234.07-.05.192-.114.369-.192.264-.118.475-.243.633-.373.158-.13.298-.276.42-.438a3.94 3.94 0 0 1 .238-.298C1.802.019 1.872 0 1.975 0c.115 0 .208.042.277.127.07.085.105.202.105.351v3.556c0 .416-.15.624-.448.624a.421.421 0 0 1-.32-.127c-.08-.085-.121-.21-.121-.376zm-.283 6.664h1.572c.156 0 .275.03.358.091a.294.294 0 0 1 .123.25.323.323 0 0 1-.098.238c-.065.065-.164.097-.296.097H.629a.494.494 0 0 1-.353-.119.372.372 0 0 1-.126-.28c0-.068.027-.16.081-.273a.977.977 0 0 1 .178-.268c.267-.264.507-.49.722-.678.215-.188.368-.312.46-.371.165-.11.302-.222.412-.334.109-.112.192-.226.25-.344a.786.786 0 0 0 .085-.345.6.6 0 0 0-.341-.553.75.75 0 0 0-.345-.08c-.263 0-.47.11-.62.329-.02.029-.054.107-.101.235a.966.966 0 0 1-.16.295c-.059.069-.145.103-.26.103a.348.348 0 0 1-.25-.094.34.34 0 0 1-.099-.258c0-.132.031-.27.093-.413.063-.143.155-.273.279-.39.123-.116.28-.21.47-.282.189-.072.411-.107.666-.107.307 0 .569.045.786.137a1.182 1.182 0 0 1 .618.623 1.18 1.18 0 0 1-.096 1.083 2.03 2.03 0 0 1-.378.457c-.128.11-.344.282-.646.517-.302.235-.509.417-.621.547a1.637 1.637 0 0 0-.148.187z"/></svg>',
         default: true,
       },
+      {
+        name: "removeTab",
+        title: this.api.i18n.t("Remove Tab"),
+        icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="17" height="13"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z"/></svg>',
+        default: false
+      },
+      {
+        name: "addTab",
+        title: this.api.i18n.t("Add Tab"),
+        icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="17" height="13"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/></svg>',
+        default: false
+      }
     ];
 
     /**
@@ -88,7 +100,10 @@ class List {
     };
 
     this.data = data;
+    this.selectedItem = null;
+    this.config = config;
   }
+  
 
   /**
    * Returns list tag with items
@@ -145,7 +160,10 @@ class List {
       },
       false
     );
-
+    this._elements.wrapper.addEventListener('click', (event) => {
+      this.selectedItem = window.getSelection().anchorNode;
+      this.selectedItem = this.selectedItem.parentElement.nodeName === 'LI' ? this.selectedItem.parentElement : this.selectedItem.parentElement.parentElement; 
+    })
     return this._elements.wrapper;
   }
 
@@ -229,7 +247,10 @@ class List {
         );
 
         // mark active
-        itemEl.classList.toggle(this.CSS.settingsButtonActive);
+        if(item.name === 'unordered' || item.name === 'ordered') {
+          itemEl.classList.toggle(this.CSS.settingsButtonActive);
+        }
+        
       });
 
       this.api.tooltip.onHover(itemEl, item.title, {
@@ -240,7 +261,7 @@ class List {
       if (this._data.style === item.name) {
         itemEl.classList.add(this.CSS.settingsButtonActive);
       }
-
+      this.setEndOfContentEditable(this.selectedItem);
       wrapper.appendChild(itemEl);
     });
 
@@ -275,25 +296,36 @@ class List {
    * @param {string} style - 'ordered'|'unordered'
    */
   toggleTune(style) {
-    this._elements.wrapper.classList.toggle(
-      this.CSS.wrapperOrdered,
-      style === "ordered"
-    );
-    this._elements.wrapper.classList.toggle(
-      this.CSS.wrapperUnordered,
-      style === "unordered"
-    );
-    const items = this._elements.wrapper.querySelectorAll("ul");
-
-    for (let i = 0; i < items.length; i++) {
-      items[i].classList.toggle(this.CSS.wrapperOrdered, style === "ordered");
-      items[i].classList.toggle(
+    if(style ==='ordered' || style ==='unordered') {
+      this._elements.wrapper.classList.toggle(
+        this.CSS.wrapperOrdered,
+        style === "ordered"
+      );
+      this._elements.wrapper.classList.toggle(
         this.CSS.wrapperUnordered,
         style === "unordered"
       );
+      const items = this._elements.wrapper.querySelectorAll("ul");
+  
+      for (let i = 0; i < items.length; i++) {
+        items[i].classList.toggle(this.CSS.wrapperOrdered, style === "ordered");
+        items[i].classList.toggle(
+          this.CSS.wrapperUnordered,
+          style === "unordered"
+        );
+      }
+      this._data.style = style;
     }
-
-    this._data.style = style;
+    else if(style === 'addTab') {
+      this.setEndOfContentEditable(this.selectedItem);
+      this.addTab();
+    }
+    else if(style === 'removeTab') {
+      if(this.selectedItem.parentNode.parentNode.tagName === "UL") {
+        this.setEndOfContentEditable(this.selectedItem);
+        this.removeTab();
+      }
+    }
   }
 
   /**
@@ -389,7 +421,7 @@ class List {
 
     return el;
   }
-
+  
   createAllElm(lidata) {
     const style =
       this._data.style === "ordered"
@@ -415,6 +447,7 @@ class List {
         );
       }
     });
+
     return ulElem;
   }
 
@@ -557,8 +590,10 @@ class List {
       this.setEndOfContentEditable(currentSelectedItem),
       false
     );
-    event.preventDefault();
-    event.stopPropagation();
+    if(event) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
   }
 
   removeTab(event) {
@@ -610,7 +645,8 @@ class List {
     } else if(this.currentItem.parentNode.parentNode.tagName === 'UL') {
       this.currentItem.parentNode.parentNode.appendChild(currentSelectedItem);
       currentSelectedItem.previousSibling.remove();
-    } else {
+    }
+     else {
       this.currentItem.parentNode.parentNode.appendChild(currentSelectedItem);
     }
     currentSelectedItem.focus();
@@ -619,8 +655,10 @@ class List {
       this.setEndOfContentEditable(currentSelectedItem),
       false
     );
-    event.preventDefault();
-    event.stopPropagation();
+    if(event) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
   }
 
   setEndOfContentEditable(contentEditableElement) {
